fdc863ae8eba3dcd5d1d858a69e2c26a05f3c0fd
Sipeng He <55742938+hsp8412@users.noreply.github.com>
2022-10-11 23:27:32 -0600

    Add project files.

---
+++ Data/JokesWebAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp.Models;
+
+namespace JokesWebApp.Data
+{
+    public class JokesWebAppDbContext : DbContext
+    {
+        public JokesWebAppDbContext (DbContextOptions<JokesWebAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokesWebApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using JokesWebApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<JokesWebAppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("JokesWebAppDbContext") ?? throw new InvalidOperationException("Connection string 'JokesWebAppDbContext' not found.")));
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<JokesWebAppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<JokesWebAppDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
